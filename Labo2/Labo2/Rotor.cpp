12959bc639e3496dd169cd43761b713794686bfe
quillasp <saralfddin93@yahoo.fr>
2019-03-11 16:35:21 +0100

    ajout des fichiers Rotor et début de l'écriture de leur comportement

--- Labo2/Labo2/Rotor.cpp
+++ Labo2/Labo2/Rotor.cpp
@@ -2,11 +2,16 @@
 
 
 
-Rotor::Rotor()
+Rotor::Rotor(unsigned id, const char notch, const std::string baseWiring)
 {
+	this->id = id;
+	this->nothc = notch;
+	this->baseWiring = baseWiring;
 }
 
-
-Rotor::~Rotor()
+void Rotor::rotation(std::string currentWiring)
 {
+	std::rotate(currentWiring.begin(), currentWiring.begin() + 1, currentWiring.end());
 }
+
+Rotor::~Rotor(){}
--- Labo2/Labo2/Rotor.h
+++ Labo2/Labo2/Rotor.h
@@ -1,8 +1,25 @@
-#pragma once
+#ifndef ROTOR_H
+#define ROTOR_H
+
+#include <string>
+#include <algorithm>
+
 class Rotor
 {
 public:
-	Rotor();
+	Rotor(unsigned id, const char notch, const std::string baseWiring);
+	void rotation(std::string currentWiring);
+	char rToL(char c);
+	char lToR(char c);
+	void setCurrentWiring(std::string baseWiring, unsigned nextPos);
 	~Rotor();
+
+private:
+	const unsigned id;
+	const char notch;
+	const std::string baseWiring;
+	std::string currentWiring;
+	int position;
 };
 
+#endif //ROTOR_H
